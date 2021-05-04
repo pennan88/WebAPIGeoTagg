1342ec46c740e27bee70a90a7a6fb3423671d154
alius <alius@LAPTOP-NNR66SG1>
2021-05-04 14:09:08 +0200

    create a path for documentation.xml.

--- WebAPIGeoTagg/Controllers/GeoMessageController.cs
+++ WebAPIGeoTagg/Controllers/GeoMessageController.cs
@@ -53,6 +53,7 @@ namespace WebAPIGeoTagg.Controllers
 
             });
         }
+        
 
     }
 }
--- WebAPIGeoTagg/Startup.cs
+++ WebAPIGeoTagg/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
 using WebAPIGeoTagg.Data;
 
 namespace WebAPIGeoTagg
@@ -28,6 +30,8 @@ namespace WebAPIGeoTagg
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIGeoTagg", Version = "v1" });
+                var path = Path.Combine(AppContext.BaseDirectory, "GeoTagging.xml");
+                c.IncludeXmlComments(path);
             });
 
             services.AddDbContext<GeoMessageDbContext>(options =>
@@ -46,6 +50,7 @@ namespace WebAPIGeoTagg
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIGeoTagg v1"));
+               
             }
 
             app.UseHttpsRedirection();
